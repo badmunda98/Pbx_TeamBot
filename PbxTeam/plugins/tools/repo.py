b352e7c252b28201fbe4e82e20db8100111fbab4
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-03-09 19:28:28 +0530

    Create repo.py

---
+++ PbxTeam/plugins/tools/repo.py
@@ -0,0 +1,71 @@
+from pyrogram import Client, filters
+import requests
+import random
+import os
+import re
+import asyncio
+import time
+from ... import *
+
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@app.on_message(
+    filters.command(["r", "repo"], ".") & (filters.me | filters.user(SUDO_USER))
+)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/3063af27d9cc8580845e1.jpg",
+        caption=f"""🦋 𝐂ʟɪᴄᴋ 𝐁ᴇʟᴏᴡ 𝐁ᴜᴛᴛᴏɴ 𝐓ᴏ 𝐆ᴇᴛ 𝐑ᴇᴘᴏ ❤️
+
+[💫 𝐑ᴇᴘᴏ 💫](https://github.com/Badhacker98/Pbx_TeamBot/fork)""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🗡️ ᴘʙx ᴛᴇᴀᴍ ʀᴇᴘᴏ 🗡️", url=f"https://github.com/Badhacker98/Pbx_TeamBot/fork"")
+                ],
+
+            ]
+        ),
+    )
+
+@app.on_message(
+    filters.command(["r", "repo"], ".") & (filters.me | filters.user(SUDO_USER))
+)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/3063af27d9cc8580845e1.jpg",
+        caption=f"""🦋 𝐂ʟɪᴄᴋ 𝐁ᴇʟᴏᴡ 𝐁ᴜᴛᴛᴏɴ 𝐓ᴏ 𝐆ᴇᴛ 𝐑ᴇᴘᴏ ❤️
+
+[💫 𝐑ᴇᴘᴏ 💫](https://github.com/Badhacker98/Pbx_TeamBot/fork)""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🗡️ ᴘʙx ᴛᴇᴀᴍ ʀᴇᴘᴏ 🗡️", url=f"https://github.com/Badhacker98/Pbx_TeamBot/fork"")
+                ],
+
+            ]
+        ),
+    )
+
+@app.on_message(
+    filters.command(["repo", "repo"], ".") & (filters.me | filters.user(SUDO_USER))
+)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/3063af27d9cc8580845e1.jpg",
+        caption=f"""🦋 𝐂ʟɪᴄᴋ 𝐁ᴇʟᴏᴡ 𝐁ᴜᴛᴛᴏɴ 𝐓ᴏ 𝐆ᴇᴛ 𝐑ᴇᴘᴏ ❤️
+
+[💫 𝐑ᴇᴘᴏ 💫](https://github.com/Badhacker98/Pbx_TeamBot/fork)""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🗡️ ᴘʙx ᴛᴇᴀᴍ ʀᴇᴘᴏ 🗡️", url=f"https://github.com/Badhacker98/Pbx_TeamBot/fork"")
+                ],
+
+            ]
+        ),
+        )
