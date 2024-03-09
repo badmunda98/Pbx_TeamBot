a84d698b29999963f61e0c07df8b7264d70d1bfb
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-03-09 12:59:21 +0530

    Update restart.py

--- PbxTeam/plugins/owner/restart.py
+++ PbxTeam/plugins/owner/restart.py
@@ -6,7 +6,7 @@ from pyrogram import filters, Client
 from ... import app, SUDO_USER
 from ... import *
 
-@app.on_message(cdz(["restart"]) && (filters.me | filters.user(SUDO_USER)))
+@app.on_message(cdz(["restart"]) & (filters.me | filters.user(SUDO_USER)))
 async def restart(client: Client, message: Message):
     reply = await message.reply_text("**Restarting...**")
     await message.delete()
