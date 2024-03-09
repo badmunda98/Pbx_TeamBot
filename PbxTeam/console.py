0a09705dd9843b0a174ab1a8701c7a1ef85ed80b
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-03-09 11:35:35 +0530

    Update console.py

--- PbxTeam/console.py
+++ PbxTeam/console.py
@@ -34,6 +34,8 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_DB_URL = getenv("MONGO_DB_URL", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
+OWNER_ID = int(getenv("OWNER_ID", "6898413162"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6898413162").split()))
 
 
 # OPTIONAL VARIABLES
