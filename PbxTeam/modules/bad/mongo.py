5bd9ecfb030206733677d0b32a3c86f966075aa7
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-03-11 16:33:24 +0530

    Update mongo.py

--- PbxTeam/modules/bad/mongo.py
+++ PbxTeam/modules/bad/mongo.py
@@ -2,7 +2,7 @@ from typing import Dict, Union
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-from config import MONGO_DB_URL
+from ... import app,  MONGO_DB_URL
 
 mongo = MongoCli(MONGO_DB_URL)
 db = mongo.Badbot
