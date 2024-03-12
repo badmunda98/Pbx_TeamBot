42c2b8928a7426427bac361e22d4ad8dc1a00e39
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-03-12 07:48:26 +0530

    Update pbiraid.py

--- PbxTeam/plugins/raids/pbiraid.py
+++ PbxTeam/plugins/raids/pbiraid.py
@@ -1,76 +1,87 @@
+rom random import choice
+from pyrogram import filters, Client
+from pyrogram.types import Message
+from PbxTeam.modules.bad.data import PBIRAID, VERIFIED_USERS, GROUP
+from ... import app, SUDO_USER, safone
 from ... import *
-from ...modules.mongo.raidzone import *
 
+ACTIVATE_RLIST = []
 
-@app.on_message(cdx(["pbr", "pbireplyraid"]))
-@sudo_users_only
-async def add_pbi_raid(client, message):
-    try:
-        aux = await eor(message, "**🔄 Processing ...**")
-        if not message.reply_to_message:
-            if len(message.command) != 2:
-                return await aux.edit(
-                    "**🤖 Reply to a user's message or give username/user_id.**"
-                )
-            user = message.text.split(None, 1)[1]
-            if "@" in user:
-                user = user.replace("@", "")
-            fulluser = await app.get_users(user)
-            user_id = fulluser.id
-        else:
-            user_id = message.reply_to_message.from_user.id
-
-        if user_id == message.from_user.id:
-            return await aux.edit(
-                "**🤣 How Fool, You Want To Activate Pbx Raid On Your Own ID❓**"
-            )
-        
-        fraid = await add_fuckraid_user(user_id)
-        if fraid:
-            return await aux.edit(
-                "**🤖 Successfully Added Pbx Raid On This User.**"
-            )
-        return await aux.edit(
-            "**🤖 Hey, Pbx Raid Already Active On This User❗**"
-        )
-    except Exception as e:
-        print("Error: `{e}`")
+@app.on_message(cdz(["pbr"])  & (filters.me | filters.user(SUDO_USER))
+)
+async def rr(client: Client, message: Message):
+    r = await message.edit_text("**Processing**")
+    reply = message.reply_to_message
+    if reply:
+        user = reply.from_user.id
+    else:
+        user = message.text.split(None, 1)[1]
+        if not user:
+            await r.edit("**Provide Me A USER_ID or reply to someone**")
+            return
+    user = await client.get_users(user)
+    if int(message.chat.id) in GROUP:
+        await r.edit("`You Cannot Spam In Developers' Chat`")
         return
-
-
-
-
-@app.on_message(cdx(["dpbr", "dpbireplyriad"]))
-@sudo_users_only
-async def del_pbi_raid(client, message):
-    try:
-        aux = await eor(message, "**🔄 Processing ...**")
-        if not message.reply_to_message:
-            if len(message.command) != 2:
-                return await aux.edit(
-                    "**🤖 Reply to a user's message or give username/user_id.**"
-                )
-            user = message.text.split(None, 1)[1]
-            if "@" in user:
-                user = user.replace("@", "")
-            fulluser = await app.get_users(user)
-            user_id = fulluser.id
-        else:
-            user_id = message.reply_to_message.from_user.id
-        
-        if user_id == message.from_user.id:
-            return await aux.edit(
-                "**🤣 How Fool, When I Activate Pbx Raid On Your ID❓**"
-            )
-        
-        fraid = await del_fuckraid_user(user_id)
-        if fraid:
-            return await aux.edit(
-                "**🤖 Successfully Removed Pbx Raid From This User.**"
-            )
-        return await aux.edit(
-            "**🤖 Hey, Pbx Raid Not Active On This User❗**"
-        )
-    except Exception as e:
-        print("Error: `{e}`")
+    if int(user.id) in VERIFIED_USERS:
+        await r.edit("You Cannot Spam On Developers")
         return
+    elif int(user.id) in SUDO_USER:
+        await r.edit("That Guy Is part of sudo user.")
+        return
+    elif int(user.id) in ACTIVATE_RLIST:
+        await r.edit("User Already in Raidlist.")
+        return
+    ACTIVATE_RLIST.append(user.id)
+    await r.edit(f"**Replyraid Activated On {user.first_name} Successfully ✅**")
+
+@app.on_message(cdz(["dpbr"])  & (filters.me | filters.user(SUDO_USER))
+)
+async def drr(client: Client, message: Message):
+    r = await message.edit_text("**Processing**")
+    reply = message.reply_to_message
+    if reply:
+        user = reply.from_user.id
+    else:
+        user = message.text.split(None, 1)[1]
+        if not user:
+            await r.edit("Provide me username/userid or reply to user for deactivating replyraid")
+            return
+    user = await client.get_users(user)
+    if int(user.id) not in ACTIVATE_RLIST:
+        await r.edit("User Not in Replyraid.")
+        return
+    ACTIVATE_RLIST.remove(user.id)
+    await r.edit(f"**Reply Raid has Been Removed {user.first_name}, enjoy!**")
+
+
+@Client.on_message(filters.incoming)
+async def watch_raids(client: Client, message: Message):
+    try:
+        if not message:
+            return
+        if not message.from_user:
+            return
+        user = message.from_user.id
+        userr = message.from_user
+        mention = f"[{userr.first_name}](tg://user?id={userr.id})"
+        pbiraid = f"{mention} {choice(PBIRAID)}"
+        if int(user) in VERIFIED_USERS:
+            return
+        elif int(user) in SUDO_USER:
+            return
+        if int(message.chat.id) in GROUP:
+            return
+        try:
+            if not message.from_user.id in ACTIVATE_RLIST:
+                return
+        except AttributeError:
+            return
+        try:
+            if message.from_user.id in ACTIVATE_RLIST:
+                await message.reply_text(pbiraid)
+        except Exception as a:
+            print(f"An error occurred (a): {str(a)}")
+    except Exception as b:
+        print(f"An error occurred (b): {str(b)}")
+    
